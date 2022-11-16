bfe74ad913be001f2d5ace6f17c4051cc61196fa
PizzaManStarted <100306838+PizzaManStarted@users.noreply.github.com>
2022-11-16 09:29:22 +0100

    added expr.py

---
+++ projet/expr.py
@@ -0,0 +1,16 @@
+from rel import Relation
+
+# Every other class in this file is a child of the expression class stocking :
+# The new Relation
+# The old Relation
+# The SQL querry created
+# If it's a Querry created from multiple expression : ex -> Project((..),rename(...))
+class Expression():
+    def __init__(self,oldRel : Relation, newRel : Relation, querry:str, isOneExp = True) -> None:
+        self.oldRel     = oldRel
+        self.newRel     = newRel
+        self.querry     = querry
+        self.isOneExp   = isOneExp
+    
+    def __str__(self) -> str:
+        return self.querry
